a85563a580eeec13830ab810f8310021dca0bb8f
henrikesilva <henrikelima.0502@gmail.com>
2024-01-13 22:53:31 -0300

    Install and configure eslint

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,16 +3,16 @@ import { z } from 'zod'
 
 // validação das variaveis de ambiente
 const envSchema = z.object({
-    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-    PORT: z.coerce.number().default(3333),
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(!_env.success){
-    console.error('❌ Invalid environment variables.', _env.error.format())
+if (!_env.success) {
+  console.error('❌ Invalid environment variables.', _env.error.format())
 
-    throw new Error('Invalid environment variables.')
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data

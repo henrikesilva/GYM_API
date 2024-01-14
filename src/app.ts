875c2e0afca1b1ed79bc5bcf5a50764617da4e02
henrikesilva <henrikelima.0502@gmail.com>
2024-01-13 23:34:27 -0300

    Add and Configure the prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Henrique Lima',
+    email: 'henrikelima.0502@gmail.com',
+  },
+})

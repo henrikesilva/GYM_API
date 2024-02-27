71b71eaa7a00c977652b7f5d3ca9e18a09338994
henrikesilva <henrikesilva.0502@gmail.com>
2024-02-27 16:58:13 -0300

    Implemented autheticate

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,5 +1,3 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
@@ -19,7 +17,20 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({ email, password })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({
+      token,
+    })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(409).send({ message: err.message })
@@ -27,6 +38,4 @@ export async function authenticate(
 
     throw err
   }
-
-  return reply.status(200).send()
 }
